1485271d4d89060212f7ac539df74244313245e8
Максим Лыков <frise.on@gmail.com>
2017-09-25 18:21:24 +0500

    add blur & remove

--- src/js/emailsEditor/emailsEditor.js
+++ src/js/emailsEditor/emailsEditor.js
@@ -17,11 +17,43 @@
 
     controller.$inject = [
         '$scope',
+        '$window'
     ];
 
     var templateUrl = require('./emailsEditor.tpl.html');
 
-    function controller($scope) {
+    function controller($scope, $window) {
         var model = this;
+
+        model.emails = [
+            {
+                _id: 0,
+                name: 'sidorov@mail.ru',
+            },
+        ];
+
+        model.onListClick = function() {
+            var inputField = $window.document.getElementById('input-field');
+            if (inputField !== undefined) {
+                inputField.focus();
+            }
+        };
+
+        model.addEmail = function() {
+            if (model.inputField.length > 0) {
+                var newId = model.emails && model.emails.length ? 
+                    Math.max.apply(null, model.emails.map(function(item) { return item._id })) + 1 :
+                    0;
+                model.emails.push({
+                    _id: newId,
+                    name: model.inputField,
+                });
+                model.inputField = '';
+            }
+        };
+
+        model.removeEmail = function(id) {
+            model.emails = model.emails.filter(function(item) { return item._id !== id });
+        }
     }
 })();
